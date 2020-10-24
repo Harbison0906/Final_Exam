275647b1a5fa1988fe7089eb49e6cd84e4d08429
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-10-23 23:25:20 -0500

    Revert "commit"
    This reverts commit 451f238378be66f91a916c0d391145d39765f72f.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,37 +1,27 @@
 import * as React from 'react';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-import Home from './views/Home';
-import Compose from './views/Compose';
-import Admin from './views/Admin';
-import Details from './views/Details';
-import Login from './views/Login';
-import Register from './views/Register';
 
+const App = (props: AppProps) => {
+	const [greeting, setGreeting] = React.useState<string>('');
 
+	React.useEffect(() => {
+		(async () => {
+			try {
+				const res = await fetch('/api/sup');
+				const greeting = await res.json();
+				setGreeting(greeting);
+			} catch (error) {
+				console.log(error);
+			}
+		})();
+	}, []);
 
+	return (
+		<div className="min-vh-100 d-flex justify-content-center align-items-center">
+			<h1 className="display-1">Sup {greeting}!</h1>
+		</div>
+	);
+};
 
+interface AppProps {}
 
-export default class App extends React.Component {
-
-
-	render() {
-		return (
-			<BrowserRouter>
-				<Link to="/">Home</Link>
-				<Link to="/compose">Compose</Link>
-				<Link to="/login">Login</Link>
-				<Link to="/register">Register</Link>
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/details/:id" component={Details} />
-					<Route exact path="/admin/:id" component={Admin} />
-					<Route exact path="/compose" component={Compose} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/register" component={Register} />
-				</Switch>
-			</BrowserRouter>
-
-		)
-	}
-
-}
+export default App;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,23 +1,10 @@
 import * as express from 'express';
-import * as passport from 'passport';
-import * as morgan from 'morgan';
-import routes from './routes';
-import './middleware/localstrategy';
-import './middleware/bearerstrategy';
-import path = require('path');
-import cors = require('cors');
-
-
+import apiRouter from './routes';
 
 const app = express();
 
-app.use(cors());
-app.use(passport.initialize());
-app.use(morgan('dev'));
-app.use(express.json());
 app.use(express.static('public'));
-app.use(routes);
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')))
+app.use(apiRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
